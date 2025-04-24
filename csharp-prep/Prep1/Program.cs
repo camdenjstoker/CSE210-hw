335a696f11df9394a2371be1042ec61720c34f6a
camdenjstoker <sto22016@byui.edu>
2025-04-24 15:00:18 -0600

    prep1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep 1 World!");
+        Console.WriteLine("");
+        Console.Write("What is you first name? ");
+        string f_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string l_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello Prep 2 World!");
+        Console.WriteLine("");
+        Console.Write("What is your percentage in the class? [Write without % sign]");
     }
 }
