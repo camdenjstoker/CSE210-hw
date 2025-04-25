f94abc1852d6597a5ce01e1067119dcd768ce349
camdenjstoker <sto22016@byui.edu>
2025-04-24 19:58:02 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
@@ -6,6 +7,48 @@ class Program
     {
         Console.WriteLine("Hello Prep 2 World!");
         Console.WriteLine("");
-        Console.Write("What is your percentage in the class? [Write without % sign]");
+        Console.Write("What is your percentage in the class? [Write without % sign]: ");
+        string value = Console.ReadLine();
+        int percent = int.Parse(value);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        } 
+        else if (percent >= 80 && percent < 90)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70 && percent < 80)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60 && percent < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your current grade is {letter}.");
+        if (letter == "A")
+        {
+            Console.WriteLine("You are passing the class. ");
+        }
+        else if (letter == "B")
+        {
+            Console.WriteLine("You are passing the class. ");
+        }
+        else if (letter == "C")
+        {
+            Console.WriteLine("You are passing the class. ");
+        }
+        else
+        {
+            Console.WriteLine("You are not passing the class. ");
+        }
+       
     }
 }
