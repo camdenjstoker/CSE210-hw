53840ccaaeda686d444a917dd8093887965b90d4
camdenjstoker <sto22016@byui.edu>
2025-04-29 15:30:36 -0600

    p3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep 3 World!");
+        Random random_generator = new Random();
+        int number = random_generator.Next(1, 100);
+        int guess;
+        string num;
+
+
+        do
+        {
+            Console.Write("Guess a number between 1-100: ");
+            num = Console.ReadLine();
+            guess = int.Parse(num);
+
+            if (guess > number)
+            {
+                Console.WriteLine("The number is lower than what you guessed.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("The number is higher than what you guessed.");
+            }
+            else 
+            {
+                Console.WriteLine("Congradulation you guessed the right number!!");
+            }
+        } while (guess != number);
     }
 }
