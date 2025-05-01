cd0653ab7c575e960cef8202cc7bad19671ab213
camdenjstoker <sto22016@byui.edu>
2025-05-01 14:23:59 -0600

    project test

---
+++ HowToDo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World.")
+
+Console.WriteLine("Example of new Project. ")
+
+Console.WriteLine("Step 1. mkdir 'ProjectName'")
+Console.WriteLine("Step 2. cd 'ProjectName;dotnet new console;cd ./. ")
+Console.WriteLine("Key: ./. = granparent level.  and ../.. parent level ")
