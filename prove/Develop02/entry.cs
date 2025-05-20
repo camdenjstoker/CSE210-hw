87eb04225c41b75d1214efa95c998b229df41dba
camdenjstoker <sto22016@byui.edu>
2025-05-20 14:17:32 -0600

    Update entry.cs

--- prove/Develop02/entry.cs
+++ prove/Develop02/entry.cs
@@ -6,6 +6,7 @@ public class Entry
    public string _Prompt;
    public string _Response;
 
+   // Formatting the entries
    public Entry(string date, string prompt, string response)
    {
     _Date = date;
@@ -13,6 +14,7 @@ public class Entry
     _Response = response;
    }
 
+   // Putting the code to work
    public override string ToString()
    {
     return $"{_Date} - {_Prompt}\n{_Response}\n";
